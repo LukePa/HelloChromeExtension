5f0085f0a378dd2a920ff28cc6c5c7d22c085b0d
luke parton <lukeparton99@gmail.com>
2023-09-16 10:40:15 +0100

    messing around

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("button");
+
+
+parapgraph = document.querySelector("p");
+
+button.addEventListener("click", () => {
+    parapgraph.textContent = "New"
+    console.log("hello")
+})
+
+
+
+
+// console.logs don't happen??
